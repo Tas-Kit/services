d3b89415ca86e7bd1081db6719008b907a6078d4
Lazy-Y <zhenyanz@usc.edu>
2018-09-05 21:17:56 -0700

    add userservice

---
+++ src/userservice.py
@@ -0,0 +1,45 @@
+"""Summary
+"""
+import requests
+from .generic import APISERVICE
+
+
+class USERSERVICE(APISERVICE):
+
+    """User Example
+    {
+        'uid': '90f5a8e3-1a43-4b4e-84c4-00167b79e477',
+        'username': 'root',
+        'first_name': '',
+        'last_name': '',
+        'birthday': None,
+        'gender': 'unknow',
+        'phone': None,
+        'address': None
+    }
+    """
+
+    SCHEME = 'http'
+    HOST = 'userservice'
+    PORT = 8000
+    API_VERSION = 'v1'
+
+    def get_user_list(self, uid_list):
+        if uid_list:
+            query = '&'.join(['uid={0}'.format(uid) for uid in uid_list])
+            url = '/users/?{0}'.format(query)
+            response = self.send_request(url, requests.get)
+            return response.json()['results']
+        return []
+
+    def get_user(self, uid=None, username=None):
+        query = []
+        if uid:
+            query.append('uid={0}'.format(uid))
+        if username:
+            query.append('username={0}'.format(username))
+        if query:
+            url = '/users/?{0}'.format('&'.join(query))
+            response = self.send_request(url, requests.get)
+            return response.json()['results'][0]
+        return []
